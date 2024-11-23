3d0759110739723c8a61a292ac4c3bba8dd440a1
VernSG <snexmania76@gmail.com>
2024-11-24 03:23:53 +0800

    fix: hero images not found
    Change-Id: Icbcb64dcc01138d1ec69f01d897436750e13760f

--- src/components/hero-video.tsx
+++ src/components/hero-video.tsx
@@ -7,14 +7,14 @@ export function HeroVideoDialogDemo() {
         className="dark:hidden block"
         animationStyle="from-center"
         videoSrc="https://www.youtube.com/embed/lI2pbqlBQkU?si=8tptNvTWViyWd0LV"
-        thumbnailSrc="http://localhost:3000/screen1.png"
+        thumbnailSrc="https://regtps-web.vercel.app/screen1.png"
         thumbnailAlt="Hero Video"
       />
       <HeroVideoDialog
         className="hidden dark:block"
         animationStyle="from-center"
         videoSrc="https://www.youtube.com/embed/lI2pbqlBQkU?si=8tptNvTWViyWd0LV"
-        thumbnailSrc="http://localhost:3000/screen1.png"
+        thumbnailSrc="https://regtps-web.vercel.app/screen1.png"
         thumbnailAlt="Hero Video"
       />
     </div>
