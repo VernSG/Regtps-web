b4f15185432853ab11995ef3f7db2b608212ae23
VernSG <snexmania76@gmail.com>
2024-11-26 07:41:07 +0800

    chore: build sitemap and robots
    Change-Id: I4f869456f351e528f35067b7a99e5fe0b040b0fc

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+// next-sitemap.js
+module.exports = {
+  siteUrl: "https://www.regtps.my.id", // Ganti dengan URL situs Anda
+  generateRobotsTxt: true, // Untuk menghasilkan file robots.txt
+};
--- next.sitemap.js
+++
@@ -1,6 +0,0 @@
-// next-sitemap.js
-module.exports = {
-  siteUrl: "https://regtps.my.id", // Ganti dengan URL situs Anda
-  generateRobotsTxt: true, // Untuk menghasilkan file robots.txt
-  sitemapSize: 7000, // Sesuaikan ukuran sitemap jika perlu
-};
